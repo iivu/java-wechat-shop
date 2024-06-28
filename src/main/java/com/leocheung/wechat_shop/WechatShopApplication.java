c2be37cf775c883d0da760e37a04abb47d9d5d86
Leo Cheung <leozhang621@gmail.com>
2024-06-28 11:54:57 +0800

    init

---
+++ src/main/java/com/leocheung/wechat_shop/WechatShopApplication.java
@@ -0,0 +1,13 @@
+package com.leocheung.wechat_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leocheung/wechat_shop/WechatShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leocheung.wechat_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
